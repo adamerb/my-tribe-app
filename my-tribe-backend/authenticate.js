67611b75c21122c448ac1f4ea6dc6dbd57b5701e
adamerb <84467063+adamerb@users.noreply.github.com>
2023-04-10 13:00:21 -0600

    Update authenticate.js
    Refactored code to remove authenticate function.
    Previously, when we authenticated, we queried the entire database and stored all users in a user array.
    Now, I am simply checking the database to see if a single user  exists, if so, they are granted a token, if not, the user gets an error message.

--- my-tribe-backend/authenticate.js
+++ my-tribe-backend/authenticate.js
@@ -1,37 +1,23 @@
 export const authenticateUser = (req, res, db) => {
     const { body } = req
     const { username, password } = body
-    let users
+    // let users
     
-    // get all users from memory
-    db.query('SELECT * FROM `testtable`', (error, results, fields) => {
-        if (error) throw error;
-
-        // loop through results and push each result into users array
-        users = results.reduce((acc, curr) => {
-            return { ...acc, [curr.UserName]: curr.Password}
-        }, {})
-
-        // check if req.username and req.password are inside our db file
-        if (authenticate(username, password, users)){
+    // See if user exists in database
+    db.query(`SELECT * FROM 'testtable' WHERE UserName='${username}' AND Password='${password}'`), (error, results, fields) => {
+        if (error) {
+            res.send({
+                error: 'Sorry your login credentials are invalid.',
+                successful: false
+            })
+            throw error;
+        } else {
             res.send({
                 authorize: 'access-token',
                 successful: true,
             })
-        } else {
-            res.send({
-                error: 'Sorry your login credentials are invalid.',
-                successful: false
-            })  
         }
-    })
-};
-
-const authenticate = (username, password, allDBUsers) => {
-    for (const [key, value] of Object.entries(allDBUsers)){
-        if (key == username && value == password){
-          return true 
-        }
+        console.log('results:',results)
+        console.log('fields',fields)
     }
-    return false
-};
+}
