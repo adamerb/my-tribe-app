db8fdc2ca3ec7436497c81fa4ac6ad48eb69985a
adamerb <84467063+adamerb@users.noreply.github.com>
2023-04-10 14:12:21 -0600

    Update authenticate.js
    Fix database query syntax.

--- my-tribe-backend/authenticate.js
+++ my-tribe-backend/authenticate.js
@@ -3,18 +3,18 @@ export const authenticateUser = (req, res, db) => {
     const { username, password } = body
     
     // See if user exists in database
-    db.query(`SELECT * FROM 'testtable' WHERE UserName='${username}' AND Password='${password}'`), (error, results, fields) => {
-        if (error) {
+    db.query(`SELECT * FROM testtable WHERE UserName='${username}' AND Password='${password}'`, (error, results, fields) => {
+        if (error || results.length === 0) {
             res.send({
                 error: 'Sorry your login credentials are invalid.',
                 successful: false
             })
-            throw error;
+            console.log(error)
         } else {
             res.send({
                 authorize: 'access-token',
                 successful: true,
             })
         }
-    }
+    })
 }
